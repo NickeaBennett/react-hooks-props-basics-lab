2f81717c18b022ff41180299023e3643c6f1776a
NickeaBennett <nickeabennett@gmail.com>
2022-05-30 11:47:21 -0400

    completed assignment

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Nickea Bennett",
+  city: "Hale Township, PA",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/NickeaBennett/",
+    linkedin: "https://www.linkedin.com/in/nickeabennett/",
   },
 };
 
